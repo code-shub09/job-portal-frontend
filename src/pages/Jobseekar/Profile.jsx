61f0d6eb3aa98393405dd42a3c7c6a4288323d7b
code-shub09 <singhshubham6135@gmail.com>
2025-11-04 20:06:55 +0530

    profile

--- src/pages/Jobseekar/Profile.jsx
+++ src/pages/Jobseekar/Profile.jsx
@@ -105,34 +105,36 @@ import axios from "axios";
 const Profile = () => {
   const [open, setOpen] = useState(false);
   const [eduList, setEduList] = useState([]);
+  const [Load, setLoad] = useState(true);
   function onEdit() {
     setOpen(true);
   }
   const [IsAddExpOpen, setIsAddExpOpen] = useState(false);
-  const [IsPreferenceModalOpen,setIsPreferenceModalOpen]=useState(false);
-  
+  const [IsPreferenceModalOpen, setIsPreferenceModalOpen] = useState(false);
+
   function onClose() {
     setIsPreferenceModalOpen(false);
   }
-  function onAdd(){
-    setIsPreferenceModalOpen(true)
-
+  function onAdd() {
+    setIsPreferenceModalOpen(true);
   }
-  useEffect(()=>{
-    async function ProfileFetch(params) {
+  useEffect(() => {
+    async function ProfileFetch() {
       try {
         const response = await axios.get(
           `https://job-portal-server-lr93.onrender.com/jobseekar/profile`,
-          { withCredentials:true,headers: { "Content-Type": "application/json" } }
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
         );
 
-       console.log(response);
-      } catch (error) {
-        
-      }
+        console.log(response);
+        setLoad(false);
+      } catch (error) {}
     }
-
-  },[])
+    ProfileFetch();
+  }, []);
   async function handleSave(edu) {
     // Option A: save locally
     setEduList((s) => [...s, edu]);
@@ -143,60 +145,65 @@ const Profile = () => {
   }
   return (
     <>
-      <div>
-        <div className="min-h-screen bg-slate-50 p-6 w-full md:w-[90%] m-auto">
-          <ProfileHeader></ProfileHeader>
+      {!Load ? (
+        <div>
+          <div>
+            <div className="min-h-screen bg-slate-50 p-6 w-full md:w-[90%] m-auto">
+              <ProfileHeader></ProfileHeader>
 
-          <div className="flex flex-col md:flex-row gap-6 justify-between">
-            <div className="flex flex-col w-[98%] md:w-[70%]  gap-4">
-              <AboutSection></AboutSection>
-              <PersonalInfoCard className="flex-1" />
-              <EducationDetails
-                education={[
-                  {
-                    type: "college",
-                    degree: "B.Tech",
-                    field: "Computer Science",
-                    institution: "Delhi University",
-                    passingYear: 2024,
-                    score: 8.6,
-                  },
-                  {
-                    type: "school",
-                    degree: "12th Grade",
-                    field: "Science",
-                    institution: "Delhi Public School",
-                    passingYear: 2020,
-                    score: 92,
-                  },
-                ]}
-                onEdit={onEdit}
-              />
-              <WorkExperience setIsAddExpOpen={setIsAddExpOpen} />
-              <CareerPreferencesCard onAdd={onAdd}/>
-              
-            </div>
-            <div className="w-full md:w-1/4 flex flex-col gap-4">
-              <SocialLinks />
-              <BoostProfileCard />
+              <div className="flex flex-col md:flex-row gap-6 justify-between">
+                <div className="flex flex-col w-[98%] md:w-[70%]  gap-4">
+                  <AboutSection></AboutSection>
+                  <PersonalInfoCard className="flex-1" />
+                  <EducationDetails
+                    education={[
+                      {
+                        type: "college",
+                        degree: "B.Tech",
+                        field: "Computer Science",
+                        institution: "Delhi University",
+                        passingYear: 2024,
+                        score: 8.6,
+                      },
+                      {
+                        type: "school",
+                        degree: "12th Grade",
+                        field: "Science",
+                        institution: "Delhi Public School",
+                        passingYear: 2020,
+                        score: 92,
+                      },
+                    ]}
+                    onEdit={onEdit}
+                  />
+                  <WorkExperience setIsAddExpOpen={setIsAddExpOpen} />
+                  <CareerPreferencesCard onAdd={onAdd} />
+                </div>
+                <div className="w-full md:w-1/4 flex flex-col gap-4">
+                  <SocialLinks />
+                  <BoostProfileCard />
+                </div>
+              </div>
             </div>
           </div>
+          {IsPreferenceModalOpen && (
+            <CareerPreferencesModal onClose={onClose}></CareerPreferencesModal>
+          )}
+
+          {IsAddExpOpen && (
+            <div className="fixed inset-0 w-screen bg-black/65 h-screen z-50 flex border border-amber-300 items-center justify-around">
+              <AddExperienceForm
+                setIsAddExpOpen={setIsAddExpOpen}
+              ></AddExperienceForm>
+            </div>
+          )}
+          <EducationModal
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            onSave={handleSave}
+          />
         </div>
-      </div>
-      {IsPreferenceModalOpen && (<CareerPreferencesModal onClose={onClose}></CareerPreferencesModal>)}
-     
-      {IsAddExpOpen && (
-        <div className="fixed inset-0 w-screen bg-black/65 h-screen z-50 flex border border-amber-300 items-center justify-around">
-          <AddExperienceForm
-            setIsAddExpOpen={setIsAddExpOpen}
-          ></AddExperienceForm>
-        </div>
-      )}
-      <EducationModal
-        isOpen={open}
-        onClose={() => setOpen(false)}
-        onSave={handleSave}
-      />
+      ):(<div>loading ....</div>)}
     </>
   );
 };
